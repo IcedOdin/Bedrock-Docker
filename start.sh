872ba01e867a7a84c94097dd8484cfdb1ab04a5b
Kana <82857100+IcedOdin@users.noreply.github.com>
2025-04-19 14:38:51 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,17 +7,20 @@ cd /bedrock
 mkdir -p logs
 LOGFILE="logs/bedrock_$(date +%Y%m%d_%H%M%S).log"
 
+mkdir -p downloads
 # Download latest Bedrock server if not present
 echo "Checking for the latest Bedrock server version..."
-LATEST_INFO=$(curl -s https://www.minecraft.net/en-us/download/server/bedrock)
-LATEST_URL=$(echo "$LATEST_INFO" | grep -oP 'https://minecraft\.azureedge\.net/bin-linux/bedrock-server-[\d\.]+\.zip' | head -n 1)
-ZIP_NAME=$(basename "$LATEST_URL")
+curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.33 (KHTML, like Gecko) Chrome/90.0.$RandNum.212 Safari/537.33" -o downloads/version.html https://minecraft.net/en-us/download/server/bedrock/
+DownloadURL=$(grep -o 'https://www.minecraft.net/bedrockdedicatedserver/bin-linux/[^"]*' downloads/version.html)
+DownloadFile=$(echo "$DownloadURL" | sed 's#.*/##')
 
-if [ ! -f "$ZIP_NAME" ]; then
-    echo "Downloading $ZIP_NAME..."
-    curl -o "$ZIP_NAME" "$LATEST_URL"
-    unzip -o "$ZIP_NAME"
-    rm "$ZIP_NAME"
+
+if [ ! -f "$DownloadFile" ]; then
+    echo "Downloading $DownloadFile..."
+    UserName=$(whoami)
+    curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.33 (KHTML, like Gecko) Chrome/90.0.$RandNum.212 Safari/537.33" -o "downloads/$DownloadFile" "$DownloadURL"
+    unzip -o "downloads/$DownloadFile"
+    rm "downloads/$DownloadFile"
     chmod +x bedrock_server
 fi
 
