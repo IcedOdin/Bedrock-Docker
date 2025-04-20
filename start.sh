3a74b29cf4ba5f219505abe411d73508e7484005
Kana <82857100+IcedOdin@users.noreply.github.com>
2025-04-19 22:16:43 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -27,6 +27,7 @@ fi
 
 # Start Minecraft Bedrock server in background
 /bedrock/bedrock_server > /bedrock/logs/server.log 2>&1 &
+echo "Bedrock Server is running....."
 
 # Start Flask API with Gunicorn (4 workers, port 5000)
 #exec gunicorn -w 4 -b 0.0.0.0:50000 main.py:app
