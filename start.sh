e8e87cb215a2e2f199e0d146985b75f8c96cddd2
Kana <82857100+IcedOdin@users.noreply.github.com>
2025-04-19 20:18:29 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -29,4 +29,4 @@ fi
 /bedrock/bedrock_server > /bedrock/logs/server.log 2>&1 &
 
 # Start Flask API with Gunicorn (4 workers, port 5000)
-exec gunicorn -w 4 -b 0.0.0.0:50000 server_api:app
+exec gunicorn -w 4 -b 0.0.0.0:50000 server-api:app
